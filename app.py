501dc86fd0e0b01f58e7f21a4162ec460ba8ec04
andvg <33574513+andvg@users.noreply.github.com>
2023-03-27 22:57:57 +0200

    dev

--- app.py
+++ app.py
@@ -27,9 +27,9 @@ def main():
         {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
         {"role": "user", "content": "Where was it played?"}
         ]
-    temperature = st.slider("Temperature", min_value=0.0, max_value=2.0, value=1,0, step=0.1, help="What sampling temperature to use, between 0 and 2. Higher values like 0.8 will make the output more random, while lower values like 0.2 will make it more focused and deterministic.")
-    top_p = st.slider("Top P", min_value=0.1, max_value=1.0, value=1,0, step=0.1, help="An alternative to sampling with temperature, called nucleus sampling, where the model considers the results of the tokens with top_p probability mass. So 0.1 means only the tokens comprising the top 10% probability mass are considered.")
-    max_tokens = st.slider("Max tokens", min_value=10, max_value=1000, value=1000, step=10, help="The maximum number of tokens to generate in the chat completion.")
+    temperature = st.slider('Temperature', min_value=0.0, max_value=2.0, value=1,0, step=0.1, help="What sampling temperature to use, between 0 and 2. Higher values like 0.8 will make the output more random, while lower values like 0.2 will make it more focused and deterministic.")
+    top_p = st.slider('Top P', min_value=0.1, max_value=1.0, value=1,0, step=0.1, help="An alternative to sampling with temperature, called nucleus sampling, where the model considers the results of the tokens with top_p probability mass. So 0.1 means only the tokens comprising the top 10% probability mass are considered.")
+    max_tokens = st.slider('Max tokens', min_value=10, max_value=1000, value=1000, step=10, help="The maximum number of tokens to generate in the chat completion.")
     if st.button("Generate"):
         st.write(messages)
 
