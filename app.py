e7e6afee7b15e4387def36ebc63841102e4020ae
andvg <33574513+andvg@users.noreply.github.com>
2023-03-27 22:24:24 +0200

    dev

--- app.py
+++ app.py
@@ -3,11 +3,21 @@ import openai
 
 openai.api_key = st.secrets["OPENAI_API_KEY"]
 
-completion = openai.ChatCompletion.create(
-  model="gpt-3.5-turbo",
-  messages=[
-    {"role": "user", "content": "Hello!"}
-  ]
-)
+def generate_text(prompt):
+    response = openai.Completion.create(
+        engine="davinci",
+        prompt=prompt,
+        temperature=0.7,
+        max_tokens=150,
+        top_p=0.9,
+        frequency_penalty=0.0,
+        presence_penalty=0.0,
+        stop=["\n"]
+    )
+    return response["choices"][0]["text"]
 
-st.json(completion.to_dict())
+def main():
+    st.title("Text Generator")
+    prompt = st.text_input("Prompt")
+    if st.button("Generate"):
+        st.write("Aieie!")
