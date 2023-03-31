a07b31df602607cb36f5789616315b76d6114ef8
andvg <33574513+andvg@users.noreply.github.com>
2023-04-01 00:23:42 +0200

    dev new

--- app.py
+++ app.py
@@ -1,38 +1,123 @@
 import streamlit as st
-import openai
+from langchain.chains import ConversationChain
+from langchain.chains.conversation.memory import ConversationEntityMemory
+from langchain.chains.conversation.prompt import ENTITY_MEMORY_CONVERSATION_TEMPLATE
+from langchain.llms import OpenAI
 
-openai.api_key = st.secrets["OPENAI_API_KEY"]
+st.set_page_config(page_title='MemoryGPT', layout='wide')
 
-def generate_text(prompt):
-    response = openai.Completion.create(
-        engine="davinci",
-        prompt=prompt,
-        temperature=0.7,
-        max_tokens=150,
-        top_p=0.9,
-        frequency_penalty=0.0,
-        presence_penalty=0.0,
-        stop=["\n"]
-    )
-    return response["choices"][0]["text"]
+if "generated" not in st.session_state:
+    st.session_state["generated"] = []
+if "past" not in st.session_state:
+    st.session_state["past"] = []
+if "input" not in st.session_state:
+    st.session_state["input"] = ""
+if "stored_session" not in st.session_state:
+    st.session_state["stored_session"] = []
 
-def main():
-    st.title("Text Generator")
-    prompt = st.text_input("Prompt")
-    models = [	"gpt-4", "gpt-4-0314", "gpt-4-32k", "gpt-4-32k-0314", "gpt-3.5-turbo", "gpt-3.5-turbo-0301"]
-    model = st.selectbox("Model", models)
-    messages = [
-        {"role": "system", "content": "You are a helpful assistant."},
-        {"role": "user", "content": "Who won the world series in 2020?"},
-        {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
-        {"role": "user", "content": "Where was it played?"}
-        ]
-    temperature = st.slider('Temperature', min_value=0.0, max_value=2.0, value=1,0, step=0.1)
-    top_p = st.slider('Top P', min_value=0.1, max_value=1.0, value=1,0, step=0.1, help="An alternative to sampling with temperature, called nucleus sampling, where the model considers the results of the tokens with top_p probability mass. So 0.1 means only the tokens comprising the top 10% probability mass are considered.")
-    max_tokens = st.slider('Max tokens', min_value=10, max_value=1000, value=1000, step=10, help="The maximum number of tokens to generate in the chat completion.")
-    if st.button("Generate"):
-        st.write(messages)
+def get_text():
+    """
+    Get the user input text.
+    Returns:
+        (str): The text entered by the user
+    """
+    input_text = st.text_input("You: ", st.session_state["input"], key="input",
+                            placeholder="Your AI assistant here! Ask me anything ...", 
+                            label_visibility='hidden')
+    return input_text
+
+def new_chat():
+    """
+    Clears session state and starts a new chat.
+    """
+    save = []
+    for i in range(len(st.session_state['generated'])-1, -1, -1):
+        save.append("User:" + st.session_state["past"][i])
+        save.append("Bot:" + st.session_state["generated"][i])        
+    st.session_state["stored_session"].append(save)
+    st.session_state["generated"] = []
+    st.session_state["past"] = []
+    st.session_state["input"] = ""
+    st.session_state.entity_memory.store = {}
+    st.session_state.entity_memory.buffer.clear()
+
+with st.sidebar.expander(" 🛠️ Settings ", expanded=False):
+    # Option to preview memory store
+    if st.checkbox("Preview memory store"):
+        st.write(st.session_state.entity_memory.store)
+    # Option to preview memory buffer
+    if st.checkbox("Preview memory buffer"):
+        st.write(st.session_state.entity_memory.buffer)
+    MODEL = st.selectbox(label='Model', options=['gpt-3.5-turbo','text-davinci-003','text-davinci-002','code-davinci-002'])
+    K = st.number_input(' (#)Summary of prompts to consider',min_value=3,max_value=1000)
+
+# Set up the Streamlit app layout
+st.title("MemoryGPT")
+st.markdown(
+        ''' 
+        > :black[**A Chatbot that remembers,**  *powered by -  [LangChain]('https://langchain.readthedocs.io/en/latest/modules/memory.html#memory') + 
+        [OpenAI]('https://platform.openai.com/docs/models/gpt-3-5') + 
+        [Streamlit]('https://streamlit.io') + [DataButton](https://www.databutton.io/)*]
+        ''')
+# st.markdown(" > Powered by -  🦜 LangChain + OpenAI + Streamlit")
+
+# Ask the user to enter their OpenAI API key
+API_O = st.sidebar.text_input(":blue[Enter Your OPENAI API-KEY :]", 
+                placeholder="Paste your OpenAI API key here (sk-...)",
+                type="password") # Session state storage would be ideal
+
+if API_O:
+    # Create an OpenAI instance
+    llm = OpenAI(temperature=0,
+                openai_api_key=API_O, 
+                model_name=MODEL, 
+                verbose=False) 
 
 
-if __name__ == "__main__":
-    main()
+    # Create a ConversationEntityMemory object if not already created
+    if 'entity_memory' not in st.session_state:
+            st.session_state.entity_memory = ConversationEntityMemory(llm=llm, k=K )
+        
+        # Create the ConversationChain object with the specified configuration
+    Conversation = ConversationChain(
+            llm=llm, 
+            prompt=ENTITY_MEMORY_CONVERSATION_TEMPLATE,
+            memory=st.session_state.entity_memory
+        )  
+else:
+    st.markdown(''' 
+        ```
+        - 1. Enter API Key + Hit enter 🔐 
+
+        - 2. Ask anything via the text input widget
+
+        Your API-key is not stored in any form by this app. However, for transparency ensure to delete your API once used.
+        ```
+        
+        ''')
+    st.sidebar.warning('API key required to try this app.The API key is not stored in any form.')
+    # st.sidebar.info("Your API-key is not stored in any form by this app. However, for transparency ensure to delete your API once used.")
+
+st.sidebar.button("New Chat", on_click = new_chat, type='primary')
+
+user_input = get_text()
+if user_input:
+    output = Conversation.run(input=user_input)  
+    st.session_state.past.append(user_input)
+    st.session_state.generated.append(output)
+
+# Allow to download as well
+download_str = []
+# Display the conversation history using an expander, and allow the user to download it
+with st.expander("Conversation", expanded=True):
+    for i in range(len(st.session_state['generated'])-1, -1, -1):
+        st.info(st.session_state["past"][i],icon="🧐")
+        st.success(st.session_state["generated"][i], icon="🤖")
+        download_str.append(st.session_state["past"][i])
+        download_str.append(st.session_state["generated"][i])
+    
+    # Can throw error - requires fix
+    download_str = '\n'.join(download_str)
+    if download_str:
+        st.download_button('Download',download_str)
+
